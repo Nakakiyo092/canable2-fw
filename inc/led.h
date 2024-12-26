6b06255abea532f5010a5b4352bc85f1e4383ab0
NAKANISHI Kiyomaro <kiyomaro092@gmail.com>
2024-12-26 18:54:30 +0900

    Revert a hardware specific modifintion

--- inc/led.h
+++ inc/led.h
@@ -6,7 +6,8 @@
 #define LED_BLUE_Port GPIOA
 #define LED_BLUE LED_BLUE_Port , LED_BLUE_Pin
 
-#define LED_GREEN_Pin GPIO_PIN_0
+#define LED_GREEN_Pin GPIO_PIN_11   // Original pinout
+//#define LED_GREEN_Pin GPIO_PIN_0    // For Walfront
 #define LED_GREEN_Port GPIOA
 #define LED_GREEN LED_GREEN_Port , LED_GREEN_Pin
 
