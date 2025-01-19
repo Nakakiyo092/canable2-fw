a72c88f82af40b8419a20d33ceb830d4f3c6f808
NAKANISHI Kiyomaro <kiyomaro092@gmail.com>
2025-01-19 15:41:17 +0900

    Revert a hardware specific modification

--- inc/led.h
+++ inc/led.h
@@ -13,10 +13,10 @@ enum led_state
 #define LED_BLUE_Port GPIOA
 #define LED_BLUE LED_BLUE_Port , LED_BLUE_Pin
 
-//#define LED_GREEN_Pin GPIO_PIN_11   // Original pinout
-//#define LED_GREEN_Port GPIOB        // Original pinout
-#define LED_GREEN_Pin GPIO_PIN_0    // For Walfront
-#define LED_GREEN_Port GPIOA        // For Walfront
+#define LED_GREEN_Pin GPIO_PIN_11   // Original pinout
+#define LED_GREEN_Port GPIOB        // Original pinout
+//#define LED_GREEN_Pin GPIO_PIN_0    // For Walfront
+//#define LED_GREEN_Port GPIOA        // For Walfront
 #define LED_GREEN LED_GREEN_Port , LED_GREEN_Pin
 
 // Duration in ms
