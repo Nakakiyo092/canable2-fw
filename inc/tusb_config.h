976cfef7b2b648a9566de74b17342198ad3bed32
NAKANISHI Kiyomaro <kiyomaro092@gmail.com>
2026-01-18 22:57:40 +0900

    Update tusb_config.h

--- inc/tusb_config.h
+++ inc/tusb_config.h
@@ -100,11 +100,11 @@
 // CDC FIFO size of TX and RX
 #define CFG_TUD_CDC_RX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
 //#define CFG_TUD_CDC_TX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
-#define CFG_TUD_CDC_TX_BUFSIZE   (4096)
+#define CFG_TUD_CDC_TX_BUFSIZE   (2048)
 
 // CDC Endpoint transfer buffer size, more is faster
 //#define CFG_TUD_CDC_EP_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
-#define CFG_TUD_CDC_EP_BUFSIZE   (4096)
+#define CFG_TUD_CDC_EP_BUFSIZE   (2048)
 
 #ifdef __cplusplus
  }
